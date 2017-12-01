539f3693909e2481618b87918cb8da4615abe3cc
Fahad Qazi <fqazi@oxgene.com>
2017-12-01 16:02:04 +0000

    adding events-list and event-thumbnail components

---
+++ app/events/events-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'events-list',
+    templateUrl: `
+       <div>
+            <h1>Upcominng Angular 2 Events</h1>
+            <hr />
+
+            <event-thumbnail
+                [event]='event1' 
+            ></event-thumbnail>
+       </div> 
+    ` 
+})
+export class EventsListComponent {
+    event1 = {
+        id: 1, 
+        name: 'Angular Connect',
+        date: '09/04/2017',
+        time: '10:00 am',
+        price: 599.99,
+        imageUrl: '/app/assets/images/angularconnect-shield',
+        location: {
+            address: '1057 DT',
+            city: 'London',
+            country: 'England'
+        }
+    }
+}
