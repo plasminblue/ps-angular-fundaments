45a96ba4c9e2a146c536eae5bb18176e23a8b8ef
Fahad Qazi <fqazi@oxgene.com>
2017-12-07 14:52:46 +0000

    adding validation to forms

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,33 +1,48 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { AuthService } from "./auth.service";
 import { Router } from "@angular/router";
 
 @Component({
-  templateUrl: "app/user/profile.component.html"
+    templateUrl: "app/user/profile.component.html",
+    styles: [
+        `
+          em {float: right; color: #E05C65; padding-left: 10px;}
+          .error input {background-color: #E3C3C5;}
+          .error {color: #999}
+    `
+    ]
 })
 export class ProfileComponent implements OnInit {
-  profileForm: FormGroup;
-  constructor(private authService: AuthService, private router: Router) {}
+    profileForm: FormGroup;
+    constructor(private authService: AuthService, private router: Router) {}
 
-  ngOnInit() {
-    let firstName = new FormControl(this.authService.currentUser.firstName);
-    let lastName = new FormControl(this.authService.currentUser.lastName);
-    this.profileForm = new FormGroup({
-      firstName: firstName,
-      lastName: lastName
-    });
-  }
+    ngOnInit() {
+        let firstName = new FormControl(
+            this.authService.currentUser.firstName,
+            Validators.required
+        );
+        let lastName = new FormControl(
+            this.authService.currentUser.lastName,
+            Validators.required
+        );
+        this.profileForm = new FormGroup({
+            firstName: firstName,
+            lastName: lastName
+        });
+    }
 
-  cancel() {
-    this.router.navigate(["events"]);
-  }
+    cancel() {
+        this.router.navigate(["events"]);
+    }
 
-  saveProfile(formValues) {
-    this.authService.updateCurrentUser(
-      formValues.firstName,
-      formValues.lastName
-    );
-    this.router.navigate(["events"]);
-  }
+    saveProfile(formValues) {
+        if (this.profileForm.valid) {
+            this.authService.updateCurrentUser(
+                formValues.firstName,
+                formValues.lastName
+            );
+            this.router.navigate(["events"]);
+        }
+    }
 }
